3c7c57d3cec0eda176d015215a04c4955c8abfce
Naser Ahadi <n.ahadi@vandar.io>
2025-08-22 23:12:13 +0330

    setting file structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    checker:({
+        eslint: {
+            lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"', // adjust as needed
+        },
+    }),
 })
